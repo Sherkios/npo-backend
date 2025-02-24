102eacc6092455209c52028887951cd8d81f0fc7
Igor <sherkiosp@gmail.com>
2025-02-24 14:06:06 +0500

    Изменил код авторизации

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
 import { AuthUserDto } from 'src/auth/dto/authUserDto';
 
 @Controller('auth')
 export default class AuthController {
   constructor(private authService: AuthService) {}
+
   @Post('login')
+  @HttpCode(200)
   async signIn(@Body() authUserDto: AuthUserDto) {
     return this.authService.signIn(
       authUserDto.login,
